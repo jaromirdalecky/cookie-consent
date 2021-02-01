75215d722c8eefc6503bd55c6a8cc9b5b9bb5391
Jaromir Dalecky <jaromir@harvestdp.com>
2021-02-02 09:47:17 +1100

    Transpile and minify output

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,26 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'index.mjs',
   output: {
-    file: 'dist/cookie-consent.js',
+    file: 'dist/cookie-consent.min.js',
     format: 'umd',
-    name: 'CookieConsent'
+    name: 'CookieConsent',
+    esModule: false,
   },
   plugins: [
     nodeResolve(),
-    commonjs()
+    commonjs(),
+    getBabelOutputPlugin({
+      plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+      ],
+      presets: [['@babel/preset-env', { modules: 'umd' }]],
+      allowAllFormats: true,
+    }),
+    terser(),
   ]
 };
